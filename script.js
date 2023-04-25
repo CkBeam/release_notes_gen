199b60302bae6dbda349e78ae505700ab9ed18b3
CkBeam <cbeam@openinsurance.com>
2023-04-25 15:28:10 -0400

    slider

--- script.js
+++ script.js
@@ -7,8 +7,18 @@ const qa = document.getElementById("qa");
 const output = document.getElementById("output");
 const generateBtn = document.getElementById("generate");
 const deploy = document.getElementById("deploy");
+const showDeployNotes = document.getElementById("show-deploy-notes");
 
 generateBtn.addEventListener("click", () => {
+	const deploySection = showDeployNotes.checked
+		? `
+<h4>Deployment:</h4>
+<ul><li>Added backup to tmp → copied to wp-content/ai1wm-backups → Changed Ownership to Daemon:Daemon</li></ul>
+<p>Time: ${deploy.value} EDT</p>
+<h4>QA Steps:</h4>
+<ul><li>${qa.value}</li></ul>`
+		: "";
+
 	const releaseNotes = `
 <h2>${currentEnv.value} TO ${targetEnv.value} Release Notes ${date.value}</h2>
 <h3 style="font-style: italic;">Release Notes:</h3> 
@@ -16,11 +26,8 @@ generateBtn.addEventListener("click", () => {
 <ul><li>${purpose.value}</li></ul> 
 <h4>Jira Task:</h4>
 <ul><li>${task.value}</li></ul> 
-<h4>Deployment:</h4>
-<ul><li>Added backup to tmp → copied to wp-content/ai1wm-backups → Changed Ownership to Daemon:Daemon</li></ul>
-<p>Time: ${deploy.value} EDT</p>
-<h4>QA Steps:</h4>
-<ul><li>${qa.value}</li></ul>
+${deploySection}
+<p><strong>Time: </strong>${deploy.value} EDT</p>
 `;
 
 	output.innerHTML = releaseNotes;
